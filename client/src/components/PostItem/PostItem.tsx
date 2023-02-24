7a1c8eb3fd35d8f23d4c7315a21438874bfb4c1e
Constantin <konstantindanilov19@hotmail.com>
2023-02-24 14:56:49 +0200

    finished postitem + categories buttons

--- client/src/components/PostItem/PostItem.tsx
+++ client/src/components/PostItem/PostItem.tsx
@@ -1,11 +1,17 @@
 import post_img from 'assets/post-img.webp';
 import { ReactComponent as EyeSVG } from 'assets/eye.svg';
 import { ReactComponent as CommentSVG } from 'assets/comment.svg';
+import { ReactComponent as EditSVG } from 'assets/edit.svg';
+import { ReactComponent as CloseSVG } from 'assets/close.svg';
 import s from './PostItem.module.scss';
 
 export const PostItem = () => {
   return (
     <div className={s.post}>
+      <div className={s.post__popupButtons}>
+        <EditSVG/>
+        <CloseSVG/>
+      </div>
       <div className={s.post__header}>
         <h1>Roast the code #1</h1>
         <h2>TS Rock paper Scissors</h2>
@@ -21,9 +27,9 @@ export const PostItem = () => {
         <div className={s.post__content__body}>
           <h2>Roast the code #1 | Rock paper Scissors</h2>
           <div className={s.post__content__body__tags}>
-          <span>#react</span>
-          <span>#fun</span>
-          <span>#typescript</span>
+            <span>#react</span>
+            <span>#fun</span>
+            <span>#typescript</span>
           </div>
           <div className={s.post__content__body__statistics}>
             <div>
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,21 +1,30 @@
 import { PostItem } from 'components/PostItem/PostItem';
+import { useState } from 'react';
+
+const categories = ['New', 'Popular']
 
 export const Home = () => {
+  const [activeCategory, setActiveCategory] = useState(0)
   return (
     <div className="home__wrapper">
       <div className="home__content">
-
         <div>
-          <div>
-            <span>New</span>
-            <span>Popular</span>
+          <div className="home__categories">
+            {categories.map((cat, ind) => (
+              <span
+                key={ind}
+                onClick={() => setActiveCategory(ind)}
+                className={activeCategory === ind ? 'active' : ''}
+              >
+                {cat}
+              </span>
+            ))}
           </div>
           <PostItem />
         </div>
 
-        <div className='tags'>TAGS</div>
-        <div className='comments'>COMMENTS</div>
-
+        <div className="tags">TAGS</div>
+        <div className="comments">COMMENTS</div>
       </div>
     </div>
   );

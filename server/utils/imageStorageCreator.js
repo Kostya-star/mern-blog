2fd1162a7bed637c45aaf80d9238f1dff005d525
Constantin <konstantindanilov19@hotmail.com>
2023-03-03 16:17:51 +0200

    testing

--- server/utils/imageStorageCreator.js
+++ server/utils/imageStorageCreator.js
@@ -1,6 +1,11 @@
+import fs from 'fs'
+
 export const imageStorageCreator = (multer) => {
   const storage = multer.diskStorage({
     destination: (_, __, cb) => {
+      if(!fs.existsSync('uploads')) {
+        fs.mkdirSync('uploads')
+      }
       cb(null, 'uploads')
     },
     filename: (_, file, cb) => {

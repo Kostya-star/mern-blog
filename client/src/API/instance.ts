c8e2a63f920b1d02c1b1146dd2844a812997703b
Constantin <konstantindanilov19@hotmail.com>
2023-03-08 11:04:56 +0200

    changed the base url

--- client/src/API/instance.ts
+++ client/src/API/instance.ts
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
 export const instance = axios.create({
-  baseURL: 'http://localhost:5000'
+  // baseURL: 'http://localhost:5000'
   // baseURL: 'https://mern-blog99.herokuapp.com/'
-  // baseURL: process.env.REACT_APP_API_URL
+  baseURL: process.env.REACT_APP_API_URL
 })
 
 instance.interceptors.request.use(config => {

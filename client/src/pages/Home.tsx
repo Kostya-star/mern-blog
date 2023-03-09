fa80ed0d99117d5d5a4f38999f436f01ad15f039
Constantin <konstantindanilov19@hotmail.com>
2023-03-09 18:08:55 +0200

    added scroll to the top button

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,16 +2,18 @@ import { Categories } from 'components/Categories/Categories';
 import { Comments } from 'components/Comments/Comments';
 import { PostItem } from 'components/PostItem/PostItem';
 import { Button } from 'components/UI/Button/Button';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { isAuthSelector } from 'redux/slices/auth';
 import { fetchComments } from 'redux/slices/comments';
 import { fetchPosts } from 'redux/slices/posts';
 import { fetchTags } from 'redux/slices/tags';
 import { useAppDispatch, useAppSelector } from './../redux/hooks';
+import { ReactComponent as ArrowUpSVG } from 'assets/arrow-up.svg';
 
 export const Home = () => {
   const dispatch = useAppDispatch();
+  const [isScrollBtnVisible, setScrollBtnVisible] = useState(false);
 
   const { posts, postsStatus, commentStatus, isComments } = useAppSelector(
     ({ posts, tags, comments }) => ({
@@ -30,6 +32,22 @@ export const Home = () => {
     dispatch(fetchPosts({ sortedCat: 'createdAt' }));
     dispatch(fetchTags());
     dispatch(fetchComments());
+  }, []);
+
+  useEffect(() => {
+    const handlePageScroll = () => {
+      if (window.pageYOffset > 300) {
+        setScrollBtnVisible(true);
+      } else {
+        setScrollBtnVisible(false);
+      }
+    };
+
+    window.addEventListener('scroll', handlePageScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handlePageScroll);
+    };
   }, []);
 
   return (
@@ -66,6 +84,17 @@ export const Home = () => {
           {postsStatus === 'error' && <div>ERROR</div>}
           {postsStatus === 'success' &&
             posts?.map((post) => <PostItem key={post._id} post={post} />)}
+        </div>
+      )}
+      {isScrollBtnVisible && (
+        <div className="home__scrollUp">
+          <Button
+            text="Scroll up"
+            className="button button_transparent"
+            onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+          >
+            <ArrowUpSVG />
+          </Button>
         </div>
       )}
     </div>

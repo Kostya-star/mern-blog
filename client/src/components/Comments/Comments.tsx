d8a65cc007fe77c0e9667a5c03413ea160be1bac
Constantin <konstantindanilov19@hotmail.com>
2023-03-09 17:23:40 +0200

    implemented the ability to hide comments

--- client/src/components/Comments/Comments.tsx
+++ client/src/components/Comments/Comments.tsx
@@ -13,9 +13,10 @@ import { createTimeSince } from 'utils/createTimeSince';
 import { MemoizedCommentItem } from './CommentItem/CommentItem';
 import s from './Comments.module.scss';
 import { ReactComponent as CloseSVG } from 'assets/close.svg';
+import { ReactComponent as ArrowDownSVG } from 'assets/arrow-down.svg';
+import { ReactComponent as ArrowUpSVG } from 'assets/arrow-up.svg';
 
-interface ICommentsProps {
-}
+interface ICommentsProps {}
 
 export const Comments: FC<ICommentsProps> = () => {
   const isAuth = useAppSelector(isAuthSelector);
@@ -30,6 +31,7 @@ export const Comments: FC<ICommentsProps> = () => {
   const dispatch = useAppDispatch();
 
   const [commentText, setCommentText] = useState({ id: '', text: '' });
+  const [isCommHidden, setCommHidden] = useState(false);
 
   const textareaRef = useRef<HTMLTextAreaElement>(null);
   const sidebarCommentsRef = useRef<HTMLDivElement>(null);
@@ -58,70 +60,88 @@ export const Comments: FC<ICommentsProps> = () => {
     dispatch(clearCommentsSlice());
   };
 
+  const onHideComments = () => {
+    setCommHidden(!isCommHidden);
+  };
+
   return (
     <div className={s.comments}>
       <div className={s.comments__header}>
         <h3>Comments</h3>
-        <span>
+        <span onClick={onHideComments}>
+          {
+            isCommHidden
+            ?
+            <ArrowDownSVG  />
+            :
+            <ArrowUpSVG  />
+          }
           <CloseSVG onClick={onCloseComments} />
         </span>
       </div>
-      {comments.map((comment) => {
-        const creationTime = createTimeSince(new Date(comment.createdAt));
+      {!isCommHidden && (
+        <>
+          {comments.map((comment) => {
+            const creationTime = createTimeSince(new Date(comment.createdAt));
 
-        return (
-          <MemoizedCommentItem
-            key={comment._id}
-            comment={comment}
-            commRef={sidebarCommentsRef}
-            creationTime={creationTime}
-            setCommentText={setCommentText}
-          />
-        );
-      })}
-
-      <div className={s.comments__create}>
-        <Avatar avatar={currentUserPhoto as string} />
-        <div className={s.comments__create__group}>
-          <div className="input">
-            <TextArea
-              placeholder="Write comment..."
-              value={commentText.text}
-              ref={textareaRef}
-              onChange={(e) => {
-                setCommentText({ ...commentText, text: e.target.value });
-                e.target.style.height = 'auto';
-                e.target.style.height = `${e.target.scrollHeight}px`;
-              }}
-            />
-            {commentText.text && !isAuth && (
-              <div className="input_error"> you are not authenticated!</div>
-            )}
-          </div>
-
-          <div className={s.comments__create__buttons}>
-            <Button
-              className={`button ${
-                commentText.text && !/^\s*$/.test(commentText.text) && isAuth
-                  ? `button_colored`
-                  : 'button_disabled'
-              }`}
-              text={commentText.id ? 'Update' : 'Comment'}
-              onClick={onSubmitComment}
-              disabled={
-                !commentText.text || /^\s*$/.test(commentText.text) || !isAuth
-              }
-            />
-            {commentText.id && (
-              <Button
-                text="Cancel update"
-                className="button button_cancel"
-                onClick={() => setCommentText({ id: '', text: '' })}
+            return (
+              <MemoizedCommentItem
+                key={comment._id}
+                comment={comment}
+                commRef={sidebarCommentsRef}
+                creationTime={creationTime}
+                setCommentText={setCommentText}
               />
-            )}
+            );
+          })}
+          <div className={s.comments__create}>
+            <Avatar avatar={currentUserPhoto as string} />
+            <div className={s.comments__create__group}>
+              <div className="input">
+                <TextArea
+                  placeholder="Write comment..."
+                  value={commentText.text}
+                  ref={textareaRef}
+                  onChange={(e) => {
+                    setCommentText({ ...commentText, text: e.target.value });
+                    e.target.style.height = 'auto';
+                    e.target.style.height = `${e.target.scrollHeight}px`;
+                  }}
+                />
+                {commentText.text && !isAuth && (
+                  <div className="input_error"> you are not authenticated!</div>
+                )}
+              </div>
+
+              <div className={s.comments__create__buttons}>
+                <Button
+                  className={`button ${
+                    commentText.text &&
+                    !/^\s*$/.test(commentText.text) &&
+                    isAuth
+                      ? `button_colored`
+                      : 'button_disabled'
+                  }`}
+                  text={commentText.id ? 'Update' : 'Comment'}
+                  onClick={onSubmitComment}
+                  disabled={
+                    !commentText.text ||
+                    /^\s*$/.test(commentText.text) ||
+                    !isAuth
+                  }
+                />
+                {commentText.id && (
+                  <Button
+                    text="Cancel update"
+                    className="button button_cancel"
+                    onClick={() => setCommentText({ id: '', text: '' })}
+                  />
+                )}
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
+        </>
+      )}
     </div>
   );
 };

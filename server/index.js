b79b17117548bb139e67248e8e0075460570e535
Constantin <konstantindanilov19@hotmail.com>
2023-03-16 00:02:50 +0200

    fixed layout for posts and comments

--- server/index.js
+++ server/index.js
@@ -35,13 +35,13 @@ mongoose.set('strictQuery', false)
 const start = async () => {
   try {
     await mongoose.connect(process.env.DB_URL)
-    // app.listen(PORT, () => console.log(`Server started on PORT${PORT}`))
+    app.listen(PORT, () => console.log(`Server started on PORT${PORT}`))
   } catch (error) {
     console.log(error);
   }
 }
 
 start()
-.then(() => {
-  app.listen(PORT, () => console.log(`Server started on PORT${PORT}`))
-})
+// .then(() => {
+//   app.listen(PORT, () => console.log(`Server started on PORT${PORT}`))
+// })

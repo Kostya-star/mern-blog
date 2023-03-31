ef56950db4318315ba44c67d14289af1690ec2bd
Constantin <konstantindanilov19@hotmail.com>
2023-03-31 23:28:46 +0300

    been working with images

--- server/controllers/image-controller.js
+++ server/controllers/image-controller.js
@@ -4,34 +4,33 @@ const uploadFile = async (req, res) => {
   try {
     const file = req.file
 
-    if(file.size > 10 * 1024 * 1024) {
+    if (file.size > 10 * 1024 * 1024) {
       return res.status(400).json({
         message: "File can't be larger than 10MB"
       })
     }
-    
-    if(!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+
+    if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
       return res.status(400).json({
         message: 'Only images are allowed'
       })
     }
-    if(!file) {
+    if (!file) {
       return res.json({
         message: 'File is not found'
       })
     }
 
-  const url = await uploadImageGoogleCloud(file)
+    const url = await uploadImageGoogleCloud(file)
 
-  res.json({
-    url
-  })
+    res.json({
+      url
+    })
 
   } catch (error) {
     console.log(error);
     res.status(500).json({
       message: 'Error when uploading the file'
-      // message: error.message
     })
   }
 }

4d57d0095444dc4ce88ca62e99fd29c3018fd924
Constantin <konstantindanilov19@hotmail.com>
2023-04-03 19:07:45 +0300

    fixed base url

--- client/src/API/baseUrl.ts
+++ client/src/API/baseUrl.ts
@@ -1,2 +1,2 @@
 // export const baseUrl = 'http://localhost:5000'
-export const baseUrl = process.env.REACT_APP_API_URL
+export const baseUrl = process.env.REACT_APP_API_URL || 'http://localhost:5000'
